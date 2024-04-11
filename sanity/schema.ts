17700e118735c20d4da9f69d11795a76a55102b9
Mich CNC <michochieng@gmail.com>
2024-04-11 11:16:39 -0700

    removed schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,10 +1,12 @@
+/*
+  This is where you should be exporting your schema files from the 
+  /schemaTypes folder
+*/
+
 import { type SchemaTypeDefinition } from 'sanity'
 
-import blockContent from './schemaTypes/blockContent'
-import category from './schemaTypes/category'
-import post from './schemaTypes/post'
-import author from './schemaTypes/author'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [post, author, category, blockContent],
+  // Add schemas here
+  types: [],
 }
